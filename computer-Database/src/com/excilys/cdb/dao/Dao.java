457d2cb62f3287711b6879c25e77a0a2c5c3b871
fwang <fwang@excilys.com>
2015-04-10 17:01:43 +0200

    Ajout de CompanyDao avec l'implementation des classes create,find,update et delete

--- computer-Database/src/com/excilys/cdb/dao/Dao.java
+++ computer-Database/src/com/excilys/cdb/dao/Dao.java
@@ -2,11 +2,11 @@ package com.excilys.cdb.dao;
 
 public abstract class Dao<T> {
 
-	public abstract T create(T obj);
+	public abstract void create(T obj);
 
 	public abstract T find(long id);
 
-	public abstract T update(T obj);
+	public abstract void update(T obj);
 
 	public abstract void delete(T obj);
 }

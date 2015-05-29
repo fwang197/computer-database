beadaae44053bd23b86d5602854d1bfd03f77aeb
fwang <fwang@excilys.com>
2015-05-29 11:42:17 +0200

    Ajout de commentaire dans le module Persistence.

--- computer-database/persistence/src/main/java/com/excilys/cdb/dao/IUserDao.java
+++ computer-database/persistence/src/main/java/com/excilys/cdb/dao/IUserDao.java
@@ -3,5 +3,13 @@ package com.excilys.cdb.dao;
 import com.excilys.cdb.model.User;
 
 public interface IUserDao {
+
+	/**
+	 * Find by user name.
+	 *
+	 * @param username
+	 *            the username
+	 * @return the user
+	 */
 	public User findByUserName(String username);
 }
